9d66983cddb83b671bd0f168e5cc2e9efcba931d
Luis Enrique Vite Aquino <luis.vite0307@gmail.com>
2016-03-06 17:21:41 -0600

    Primera parte

--- Farmacia.c
+++ Farmacia.c
@@ -1,22 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
 int Menu(){
-
 	int opción,dato;
-
 	do{		
-
 		printf("%s",texto);
 		scanf("%d",&opcion);
-
 		if (opcion<1 || opcion>n){
-
 			printf("Error:la opcion seleccionada no es valida");
 		}
-	}while(opcion<1||opcion>n);
-
+	}while(opcion<1 || opcion>n);
 	return opcion;
 }
