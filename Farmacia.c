4335ff0311a79a0a8d85b5dc64b53f1bd13f9a84
Luis Enrique Vite Aquino <luis.vite0307@gmail.com>
2016-03-07 12:08:19 -0600

    2da modificacion

--- Farmacia.c
+++ Farmacia.c
@@ -1,12 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX
 
-int Menu(){
+int arreglo[MAX];   /*Se declaran las variables globales, estas seran cambiadas mas adelante por una estructura para poder manejar memoria dinamica*/
+int indice;
+
+/*Aqui seran declaradas los prototipos de nuestras funciones*/
+int menu(char texto[], int n);
+
+
+/*Este sera el main*/
+int main()
+{
+	int opcion;
+	indice = -1;
+
+	do{
+		opcion = menu("1) Agregar\n2) Borrar\n3) Consultar\n 4)Salir\n",4);
+		switch(opcion)
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+		}
+	}while(opcion!=7)
+}
+
+/*Apartir de aqui ya declararemos nuestros metodos*/
+int Menu(char texto[], int n)
+{
 	int opción,dato;
-	do{		
+	do
+	{		
 		printf("%s",texto);
 		scanf("%d",&opcion);
-		if (opcion<1 || opcion>n){
+		if (opcion<1 || opcion>n)
+		{
 			printf("Error:la opcion seleccionada no es valida");
 		}
 	}while(opcion<1 || opcion>n);
